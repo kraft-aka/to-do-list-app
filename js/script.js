17d18e45737056f83aa3fd5feb340b6f3f3fc0c7
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-03-15 22:22:14 +0100

    wrap the codes in iife and prettify

--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
-
-// add item to a list
-function newItem() {
-    let inputValue = $('#input[name=ListItem]').val();
+(function () {
+    // add item to a list
+    function newItem() {
+        let inputValue = $('#input[name=ListItem]').val();
         if (!inputValue) {
             alert('Please, enter an item!');
-    } else {
-        $('#list').append('<li>'+ inputValue +'</li>');
-    };   
-}
+        } else {
+            $('#list').append('<li>' + inputValue + '</li>');
+        };
+    }
 
 
-// crossing out list items
-$('ol').on('click','li', function() {
-      $(this).toggleClass('strike');
-});
+    // crossing out list items
+    $('ol').on('click', 'li', function () {
+        $(this).toggleClass('strike');
+    });
 
 
-// delete an item
-$('ol').on('dblclick','li', function() {
-   $(this).toggleClass('delete');
-});
+    // delete an item
+    $('ol').on('dblclick', 'li', function () {
+        $(this).toggleClass('delete');
+    });
 
 
-// function to move items of the list
-$(function() {
-    $('ol').sortable();
-});
+    // function to move items of the list
+    $(function () {
+        $('ol').sortable();
+    });
 
 
-// focus to change background color of input 
-$('input').focus(function () {
-    $(this).css('background', '#fdeff4');
-});
+    // focus to change background color of input 
+    $('input').focus(function () {
+        $(this).css('background', '#fdeff4');
+    });
 
 
-// blur the input back to white
-$('input').blur(function () {
-    $(this).css('background', 'white');
-});
+    // blur the input back to white
+    $('input').blur(function () {
+        $(this).css('background', 'white');
+    });
+
+})();
+
 
 
 
