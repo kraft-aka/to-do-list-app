0c9cee805443a0e6ce65710589b50a44c40aa6b8
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-03-15 23:00:41 +0100

    delete button in items // in progress

--- js/script.js
+++ js/script.js
@@ -1,45 +1,52 @@
-(function () {
-    // add item to a list
-    function newItem() {
-        let inputValue = $('#input[name=ListItem]').val();
-        if (!inputValue) {
-            alert('Please, enter an item!');
-        } else {
-            $('#list').append('<li>' + inputValue + '</li>');
-        };
-    }
 
 
-    // crossing out list items
-    $('ol').on('click', 'li', function () {
-        $(this).toggleClass('strike');
-    });
+
+// add item to a list
+function newItem() {
+    let inputValue = $('#input[name=ListItem]').val();
+    if (!inputValue) {
+        alert('Please, enter an item!');
+    } else {
+        $('#list').append('<li>' + inputValue + '</li>');
+    };
+}
+
+let li = $('<li></li>');
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
 
 
-    // delete an item
-    $('ol').on('dblclick', 'li', function () {
-        $(this).toggleClass('delete');
-    });
+// crossing out list items
+$('ol').on('click', 'li', function () {
+    $(this).toggleClass('strike');
+});
 
 
-    // function to move items of the list
-    $(function () {
-        $('ol').sortable();
-    });
+// delete an item
+$('ol').on('dblclick', 'li', function () {
+    $(this).toggleClass('delete');
+});
 
 
-    // focus to change background color of input 
-    $('input').focus(function () {
-        $(this).css('background', '#fdeff4');
-    });
+// function to move items of the list
+$(function () {
+    $('ol').sortable();
+});
 
 
-    // blur the input back to white
-    $('input').blur(function () {
-        $(this).css('background', 'white');
-    });
+// focus to change background color of input 
+$('input').focus(function () {
+    $(this).css('background', '#fdeff4');
+});
 
-})();
+
+// blur the input back to white
+$('input').blur(function () {
+    $(this).css('background', 'white');
+});
+
+
 
 
 
