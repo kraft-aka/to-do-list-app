a9b5ee7adb424a792e62c7ca35f159a1b00e6b77
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-03-16 09:34:12 +0100

    add delete button and function

--- js/script.js
+++ js/script.js
@@ -1,50 +1,62 @@
 
-
-
-// add item to a list
 function newItem() {
-    let inputValue = $('#input[name=ListItem]').val();
+
+
+    // Create variables
+    let listItem = $('<li></li>');
+    let inputValue = $('#input').val();
+
+
+    // Check if item passed to input 
     if (!inputValue) {
         alert('Please, enter an item!');
-    } else {
-        $('#list').append('<li>' + inputValue + '</li>');
     };
+
+    // Append item to a list
+    listItem.append(inputValue);
+
+
+    // Add item to the list as to-do item
+    $('#list').append(listItem);
+
+
+    // Delete button
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    listItem.append(crossOutButton);
+
+
+    // Delete item function
+    crossOutButton.on('click', function () {
+        listItem.addClass('delete');
+    });
+
+
+    // Crossing out list items
+    listItem.on('dblclick', function () {
+        listItem.toggleClass('strike');
+    });
+
+
+    // Function to move items of the list
+    $(function () {
+        $('ol').sortable();
+    });
+
+
+    // Focus to change background color of input 
+    $('input').focus(function () {
+        $(this).css('background', '#fdeff4');
+    });
+
+
+    // Blur the input back to white
+    $('input').blur(function () {
+        $(this).css('background', 'white');
+    });
+
 }
 
-let li = $('<li></li>');
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-
-// crossing out list items
-$('ol').on('click', 'li', function () {
-    $(this).toggleClass('strike');
-});
-
-
-// delete an item
-$('ol').on('dblclick', 'li', function () {
-    $(this).toggleClass('delete');
-});
-
-
-// function to move items of the list
-$(function () {
-    $('ol').sortable();
-});
-
-
-// focus to change background color of input 
-$('input').focus(function () {
-    $(this).css('background', '#fdeff4');
-});
-
-
-// blur the input back to white
-$('input').blur(function () {
-    $(this).css('background', 'white');
-});
 
 
 
